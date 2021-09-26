b1f51a60531dd62a72776d054eeda3b318c17f07
thecatmiaw <58418706+thecatmiaw@users.noreply.github.com>
2021-09-26 19:15:35 +0200

    Dispersion rate works

--- FallingSandSimulation/FallingSandSimulation/Source/Elements/Abstract elements/Liquid.cpp
+++ FallingSandSimulation/FallingSandSimulation/Source/Elements/Abstract elements/Liquid.cpp
@@ -9,8 +9,10 @@ Liquid::~Liquid()
 {
 }
 
+//TODO: add diagonal support.
 void Liquid::UpdateElement(Simulation* sim)
 {
+	//if under is empty go here first
 	if (sim->GetElementTag(posX, posY + 1) == ElementTag::EMPTY)
 	{
 		SwapPositions(sim, posX, posY + 1);
@@ -19,23 +21,60 @@ void Liquid::UpdateElement(Simulation* sim)
 	{
 		int direction = rand() % 2 + 1;
 
-		for (int i = 1; i <= dispersionRate; i++)
+		//Check all the way in the choosen direction, until it finds a not empty element
+
+		//TODO maybe this is cursed
+		if (dispersionRate > 1)
+		{
+			for (int i = 1; i <= dispersionRate; i++)
+			{
+				if (direction == 1)
+				{
+					//If it's at the end of the loop, and still hasn't found a filled element, then it just chooses this one.
+					if (sim->GetElementTag(posX - i, posY) == ElementTag::EMPTY && i < dispersionRate)
+					{
+						continue;
+					}
+					else
+					{
+						SwapPositions(sim, posX - (i - 1), posY);
+
+						break;
+					}
+				}
+				else
+				{
+					if (sim->GetElementTag(posX + i, posY) == ElementTag::EMPTY && i < dispersionRate)
+					{
+						continue;
+					}
+					else
+					{
+						SwapPositions(sim, posX + (i - 1), posY);
+
+						break;
+					}
+				}
+			}
+		}
+		else
 		{
 			if (direction == 1)
 			{
-				if (sim->GetElementTag(posX - i, posY) == ElementTag::EMPTY)
+				if (sim->GetElementTag(posX - 1, posY) == ElementTag::EMPTY )
 				{
-					SwapPositions(sim, posX - i, posY);
+					SwapPositions(sim, posX - 1, posY);
 				}
 			}
 			else
 			{
-				if (sim->GetElementTag(posX + i, posY) == ElementTag::EMPTY)
+				if (sim->GetElementTag(posX + 1, posY) == ElementTag::EMPTY )
 				{
-					SwapPositions(sim, posX + i, posY);
+					SwapPositions(sim, posX + 1, posY);
 				}
 			}
 		}
+		
 
 		
 	}
